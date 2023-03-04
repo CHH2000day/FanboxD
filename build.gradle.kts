410438421de86e9de60f3061bb695bd951fd1d59
CHH2000day <chh1932383611@gmail.com>
2023-03-04 21:15:59 +0800

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,8 @@
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTargetWithHostTests
 
 plugins {
-    kotlin("multiplatform") version "1.8.0"
-    kotlin("plugin.serialization") version "1.8.0"
+    kotlin("multiplatform") version "1.8.10"
+    kotlin("plugin.serialization") version "1.8.10"
 }
 
 group = "com.chh2000day.app"
@@ -62,7 +62,7 @@ kotlin {
                 implementation("io.ktor:ktor-client-core:2.2.3")
                 implementation("io.ktor:ktor-client-content-negotiation:2.2.3")
                 implementation("io.ktor:ktor-serialization-kotlinx-json:2.2.3")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0-RC")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
                 implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
                 implementation("co.touchlab:kermit:1.2.2")
             }
